e2efcdc9b1d36974ff31ca29ffe63cccd26bcc2e
Matteo Guindani <matteo.guindani@businesscreate.com>
2013-04-08 11:48:54 +0200

    Refs #3039

--- component/frontend/views/containers/tmpl/default.php
+++ component/frontend/views/containers/tmpl/default.php
@@ -1,4 +1,5 @@
-<?php defined('_JEXEC') or die('The way is shut');
+<?php
+
 /**
  * @version			  $Id: views/containers/tmpl/default.php 2012-08-18 22:50:00Z zanardi $
  * @package			  GiBi PizzaBox
@@ -9,38 +10,34 @@
  * @license			  GNU/GPL v2 or later
  */
 
-$document =& JFactory::getDocument();
-$document->addScript( JURI::base() . "/components/com_pizzabox/assets/helper.js" );
-$this->loadHelper('pizzabox');
-$helper = new PizzaboxHelper();
+defined('_JEXEC') or die('The way is shut');
+JHTML::_('behavior.tooltip');
+JHTML::script('helper.js', JURI::base() . '/components/com_pizzabox/assets/');
+
 ?>
 <h1><?php echo JText::_('PIZZABOX_CONTAINER_SELECT') ?></h1>
 <form action="index.php?option=com_pizzabox&controller=containers" method="post" name="containerForm" id="containerForm">
   <div class="buttons">
     <input type="submit" class="btn btn-primary" value="<?php echo JText::_('JNEXT') ?>" />
-    <input type="hidden" name="task" value="save" /> 
+    <input type="hidden" name="task" value="save" />
     <input type="hidden" name="element" value="container" />
-    <input type="hidden" name="ordering" value="ordering" />
-    <input type="hidden" name="filter_order" value="<?php echo $this->order ?>" />
-    <input type="hidden" name="filter_order_Dir" value="<?php echo $this->order_dir ?>" />
-    <input type="hidden" name="limitstart" value="<?php echo $this->limitstart ?>" />
-    <?php echo JHTML::_( 'form.token' ) ?>
+    <?php echo JHTML::_('form.token') ?>
   </div>
   <div id="radiobox-wrapper" class="clearfix">
-    <?php foreach ($this->rows as $k => $row) :?>
-      <div class="radiobox clearfix radio_<?php echo $row->id ?>">
-        <input type="radio" id="radio_<?php echo $row->id ?>" name="containers" value="<?php echo $row->id ?>"/> 
-        <label for="radio_<?php echo $row->id ?>">
-          <?php if( $row->image ): ?>
-            <span class="image"><img src="<?php echo $row->image ?>" alt="<?php echo $row->desc ?>" /></span>
-          <?php endif ?>
-          <span class="name lead"><?php echo $row->name ?></span>
-          <?php if( $row->price > 0 ) : ?>
-            <span class="price"><?php echo $helper->formatPrice( $row->price ) ?></span>
-          <?php endif ?>
-          <span class="desc"><?php echo $row->desc ?></span>
-        </label>
-      </div>
+    <?php foreach ($this->containers as $container) :?>
+		<div class="radiobox clearfix radio_<?php echo $container->id ?>">
+			<input type="radio" id="radio_<?php echo $container->id ?>" name="containers" value="<?php echo $container->id ?>"/>
+			<label for="radio_<?php echo $container->id ?>">
+				<?php if( $container->image ): ?>
+					<span class="image"><img src="<?php echo $container->image ?>" alt="<?php echo $container->desc ?>" /></span>
+				<?php endif ?>
+				<span class="name lead"><?php echo $container->name ?></span>
+				<?php if ($container->price > 0): ?>
+					<span class="price"><?php echo $this->helper->formatPrice($container->price) ?></span>
+				<?php endif ?>
+				<span class="desc"><?php echo $container->desc ?></span>
+			</label>
+		</div>
     <?php endforeach ?>
   </div>
 </form>
--- component/frontend/views/containers/view.html.php
+++ component/frontend/views/containers/view.html.php
@@ -1,4 +1,5 @@
-<?php defined('_JEXEC') or die('The way is shut');
+<?php
+
 /**
  * @version		    $Id: views/containers/view.html.php 2012-09-02 14:14:00Z zanardi $
  * @package		    GiBi PizzaBox
@@ -9,40 +10,20 @@
  * @license		    GNU/GPLv2
  */
 
+defined('_JEXEC') or die('The way is shut');
 jimport( 'joomla.application.component.view');
 
+/**
+ * PizzaboxViewContainers
+ */
 class PizzaboxViewContainers extends JView
-
 {
-	var $pagination = null ;
-	var $user = null;
-
-	function display( $tpl = null )
+	public function display($tpl = null)
 	{
-		jimport( 'joomla.html.pagination' );
-		JHTML::_('behavior.tooltip');
-		$app 		=& JFactory::getApplication();
-		$this->user	=& JFactory::getUser();
-		$context = 'com_pizzabox.containers.';
-		
-		// get filter and search status
-		$filters['order']		= $app->getUserStateFromRequest( $context.'filter_order', 'filter_order', 'ordering', 'cmd' );
-		$filters['order_Dir']	= $app->getUserStateFromRequest( $context.'filter_order_Dir', 'filter_order_Dir', '', 'word' );
-		$search					= $app->getUserStateFromRequest( $context.'search', 'search', '', 'string' );
-		$search 				= JString::strtolower($search);
-		
-		// sanitize $filter_order
-		if (!in_array($filters['order'], array('ordering', 'id'))) {
-			$filters['order'] = 'ordering';
-		}
-		
-		$model = $this->getModel();
-		$items = $this->get('items');
-		$this->assign( $items );
-		$this->pagination = new JPagination($items['total'], $items['limitstart'], $items['limit']);
+		$this->containers = $this->getModel()->getListNotEmpty();
 
-		$params =& JComponentHelper::getParams('com_pizzabox');
-		$this->assignRef('params' , $params );
+		$this->loadHelper('pizzabox');
+		$this->helper = new PizzaboxHelper();
 
 		parent::display($tpl);
 	}
